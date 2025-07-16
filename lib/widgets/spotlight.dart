b8919917ddf20430a3a4d6466744dec11c32f035
Karim Gaber <karim@mvp-apps.ae>
2025-07-16 12:17:38 +0400

    Refactor Spotlight widget to wrap CustomPaint in GestureDetector for tap absorption

--- lib/widgets/spotlight.dart
+++ lib/widgets/spotlight.dart
@@ -14,11 +14,14 @@ class Spotlight extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      size: MediaQuery.of(context).size,
-      painter: _SpotlightPainter(
-        target: targetRect.inflate(padding),
-        color: overlayColor,
+    return GestureDetector(
+      onTap: () {}, // absorb taps
+      child: CustomPaint(
+        size: MediaQuery.of(context).size,
+        painter: _SpotlightPainter(
+          target: targetRect.inflate(padding),
+          color: overlayColor,
+        ),
       ),
     );
   }
